51ad936ee64f1aa85c895ec1c3a56a687a72d9d0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-24 11:55:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import classes from './App.module.css';
+import Person from './Person/Person';
 
 function App() {
+  const [personState, setPersonState] = useState({
+    persons: [
+      { id: 'dfghyj', name: 'Gagan', age: 0 },
+      { id: 'dgfhj', name: 'Ralts', age: 1 },
+      { id: 'dfxgthy', name: 'Mewtwo', age: 2 },
+      { id: 'cdfvgb', name: 'Gallade', age: 3 },
+    ],
+    otherState: 'Lol xD',
+  });
+
+  // Will be executed whenever data is entered in text box
+  const nameChangeHandler = (event, id) => {
+    const personIndex = personState.persons.findIndex((p) => p.id === id);
+    const person = { ...personState.persons[personIndex] };
+    person.name = event.target.value;
+    const persons = [...personState.persons];
+    persons[personIndex] = person;
+
+    setPersonState({
+      persons: persons,
+    });
+    console.log('Name change Method executed');
+  };
+
+  const [showHideState, setShowHideState] = useState({
+    buttonState: false,
+  });
+
+  const showHideButtonHandler = (event) => {
+    console.log('Toggled show hide');
+    setShowHideState({
+      buttonState: !showHideState.buttonState,
+    });
+  };
+
+  // Added a sdelete button hadler
+  const deleteButtonHandler = (personIndex) => {
+    // Ass a good prcatie we must always update the arryain immute manner i.e without changing inital state
+    const persons = [...personState.persons];
+    persons.splice(personIndex, 1);
+    setPersonState({ persons: persons });
+  };
+
+  const style = {
+    backgroundColor: 'green',
+    color: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer',
+    // Requies radium package
+    // Must be inside single quotes as they are not valid css properties in vanilla react
+    ':hover': {
+      backgroundColor: 'salmon',
+      color: 'black',
+    },
+  };
+
+  let btnClass = [classes.Button];
+  let personView = null;
+  if (showHideState.buttonState) {
+    personView = (
+      <div>
+        {personState.persons.map((person, index) => {
+          return (
+            <Person
+              key={person.id}
+              change={(event) => nameChangeHandler(event, person.id)}
+              name={person.name}
+              age={person.age}
+              deleteButton={() => deleteButtonHandler(index)}
+            />
+          );
+        })}
+      </div>
+    );
+    btnClass.push(classes.Red);
+  } else personView = null;
+
+  let assignedClasses = [];
+  if (personState.persons.length <= 2)
+    assignedClasses = ['red', 'bold'].join(' ');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <button className={btnClass.join(' ')} onClick={showHideButtonHandler}>
+        Show/Hide Persons
+      </button>
+      {personView}
     </div>
   );
 }
 
+// export default Radium(App);
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Person.module.css';
+
+const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px',
+    },
+  };
+  return (
+    // Css Module
+    <div className={classes.Person}>
+      <p> I'm a Person!</p>
+      <p>
+        Name provided as attribute: {props.name}, age: {props.age}{' '}
+      </p>
+      <p>Age {Math.floor(Math.random() * 100)}</p>
+      <p> {props.children} </p>
+      <button onClick={props.deleteButton}>Delete</button>
+      <br />
+      <input type='text' onChange={props.change} />
+    </div>
+  );
+};
+
+export default person;
